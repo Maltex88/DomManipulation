21bea081a70de1a6ed569fbf8aed3010bffcdd84
unknown <henrik.nilsson88@gmail.com>
2018-12-02 23:02:55 +0100

    12-02

---
+++ dom.js
@@ -0,0 +1,37 @@
+/*console.dir(document); ger oss allt i DOMen, allt som finns på sidan.*/
+
+/*console.log(document.Olika commande för delar av hemsidan t.ex document.title);*/
+
+
+/*console.log(document.all); /*Ger ett indexvärde för alla element på hemsidan*/
+/*document.all[8].textContent = 10; /*Väljer ut element baserat på deras index*/
+
+/*html selektorer*/
+//console.log(document.getElementById('tryoutH3'));
+//var tryoutP = document.getElementById('tryoutP')
+//console.log(tryoutH);
+//tryoutH.style.backgroundColor = 'gray';
+/*html selektorer*/
+//tryoutP.style.border = '3x solid black';
+/*
+var items = document.getElementsByClassName('listGrp')
+/*
+console.log(items);
+items[1].textContent = 'Hello 2'
+items[1].style.fontWeight = 'bold';
+items[2].style.backgroundColor = 'Yellow';
+*/
+/*
+for(var i = 0; i < items.length; i++) {
+  items[i].style.backgroundColor = 'yellow';
+}*/
+/*var li = document.getElementsByTagName('li');
+console.log(li);
+
+for(i = 0; i < li.length; i++) {
+  li[i].style.backgroundColor = 'yellow';
+}*/
+var hej = document.getElementById('ass1');
+console.log(hej);
+hej.innerHTML = 'Detta är den nya diven';
+;
